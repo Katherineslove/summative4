dd0dd393b5bf7d5e9c115c1b7c049a6400d29248
Katherine Love <katherine@Katherines-MacBook-Air.local>
2019-11-26 16:00:04 +1300

    editing the footer and headers

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<?php if(has_nav_menu( 'bottom_navigation' )): ?>
+<!-- <?php if(has_nav_menu( 'bottom_navigation' )): ?>
     <footer class="navColour p-5 mt-5">
         <div class="container">
             <div class="row">
@@ -8,6 +8,29 @@
                         'container'       => '',
                         'menu_class'      => 'footerNav'
                     )); ?>
+                </div>
+            </div>
+        </div>
+    </footer>
+<?php endif; ?>
+
+<?php wp_footer(); ?>
+</body>
+</html> -->
+
+<?php if(has_nav_menu( 'bottom_navigation' )): ?>
+    <footer class="bg-dark text-white p-5 mt-5 my-theme">
+        <div class="container">
+            <div class="row">
+                <?php wp_nav_menu( array(
+                    'theme_location' => 'bottom_navigation',
+                    'container' => '',
+                    'menu_class' => 'footerNav'
+                )); ?>
+            </div>
+            <div class="row">
+                <div class="col text-center">
+                    <p><?php echo get_theme_mod('1902_footerMessage'); ?></p>
                 </div>
             </div>
         </div>
--- header.php
+++ header.php
@@ -10,3 +10,36 @@
     <?php wp_head(); ?>
 </head>
 <body>
+
+    <div class="navigation">
+        <h1>this is the nav</h1>
+    </div>
+
+
+    <!-- <?php if(has_nav_menu( 'top_navigation' )): ?>
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark navColour" role="navigation">
+            <div class="container">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#top_navigation" aria-controls="top_navigation" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <a class="navbar-brand" href="#"><?php echo get_bloginfo('name'); ?></a>
+                <?php
+                wp_nav_menu( array(
+                    'theme_location'    => 'top_navigation',
+                    'depth'             => 2,
+                    'container'         => 'div',
+                    'container_class'   => 'collapse navbar-collapse navText',
+                    'container_id'      => 'top_navigation',
+                    'menu_class'        => 'nav navbar-nav',
+                    'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+                    'walker'            => new WP_Bootstrap_Navwalker(),
+                ) );
+                ?>
+            </div>
+            <form action="<?php echo home_url();?>" method="get" class="d-flex justify-content-center">
+                <input type="hidden" name="post_type" value="post, movie"></input>
+                <input name="s" type="text" class="form-control" aria-describedby="searchPosts" placeholder="Search Posts" value="<?php the_search_query(); ?>">
+                <button class="btn text-white ml-3 searchButton" type="submit">Search</button>
+            </form>
+        </nav>
+    <?php endif; ?> -->
