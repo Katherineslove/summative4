83d4f91a6aa5b1fdac0e9497598d3f4e2f8cea5d
Katherine Love <katherine@Katherines-MacBook-Air.local>
2019-11-28 09:06:59 +1300

    styled the nav

--- aboutPage-template.php
+++ aboutPage-template.php
@@ -8,21 +8,23 @@ Template Post Type: page
 <?php get_header(); ?>
 
 <div class="container-fluid p-0">
-    <div class="headerImage">
+    <div class="aboutImage">
         <?php if(has_nav_menu( 'top_navigation' )): ?>
             <nav class="navbar navbar-expand-md navbar-light topNAV p-0" role="navigation">
-            <div class="container p-0">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#top_navigation" aria-controls="top_navigation" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="logo mr-2">
+                <div class="logo">
                     <?php if (get_theme_mod('2019logoImageDisplay')): ?>
-                        <img src="<?php echo get_theme_mod('2019logoImageDisplay');  ?>"class="logo w-100">
+                        <img src="<?php echo get_theme_mod('2019logoImageDisplay');  ?>"class="logoImage img-fluid">
                     <?php else: ?>
                         <h1 class="siteName"><?php echo get_bloginfo('name'); ?></h1>
                     <?php endif; ?>
                 </div>
+
+                <button class="navbar-toggler button" type="button" data-toggle="collapse" data-target="#top_navigation" aria-controls="top_navigation" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+
                 <div class="links">
                     <?php
                     wp_nav_menu( array(
@@ -37,13 +39,13 @@ Template Post Type: page
                     ) );
                     ?>
                 </div>
-            </div>
-            <form action="<?php echo home_url();?>" method="get" class="d-flex justify-content-center">
-                <input type="hidden" name="post_type" value="post, movie"></input>
-                <input name="s" type="text" class="form-control" aria-describedby="searchPosts" placeholder="Search Posts" value="<?php the_search_query(); ?>">
-                <button class="btn text-black ml-3 searchButton mr-4" type="submit">Search</button>
-            </form>
-        </nav>
+
+                <form action="<?php echo home_url();?>" method="get" class="d-flex justify-content-center searchSection">
+                    <input type="hidden" name="post_type" value="post, movie"></input>
+                    <input name="s" type="text" class="form-control border border-dark" aria-describedby="searchPosts" placeholder="Search Posts" value="<?php the_search_query(); ?>">
+                    <button class="btn text-black ml-3 searchButton mr-4 border border-dark" type="submit">Search</button>
+                </form>
+            </nav>
         <?php endif; ?>
     </div>
 
--- front-page.example.php
+++ front-page.example.php
@@ -4,72 +4,63 @@
     <div class="headerImage">
         <?php if(has_nav_menu( 'top_navigation' )): ?>
             <nav class="navbar navbar-expand-md navbar-light topNAV p-0" role="navigation">
-            <div class="container p-0">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#top_navigation" aria-controls="top_navigation" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+                <div class="container p-0">
+                    <div class="logo mr-2">
+                        <?php if (get_theme_mod('2019logoImageDisplay')): ?>
+                            <img src="<?php echo get_theme_mod('2019logoImageDisplay');  ?>"class="logo w-100">
+                        <?php else: ?>
+                            <h1 class="siteName"><?php echo get_bloginfo('name'); ?></h1>
+                        <?php endif; ?>
+                    </div>
 
-                <div class="logo mr-2">
-                    <?php if (get_theme_mod('2019logoImageDisplay')): ?>
-                        <img src="<?php echo get_theme_mod('2019logoImageDisplay');  ?>"class="logo w-100">
-                    <?php else: ?>
-                        <h1 class="siteName"><?php echo get_bloginfo('name'); ?></h1>
-                    <?php endif; ?>
+                    <button class="navbar-toggler button" type="button" data-toggle="collapse" data-target="#top_navigation" aria-controls="top_navigation" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+
+                    <div class="links">
+                        <?php
+                        wp_nav_menu( array(
+                            'theme_location'    => 'top_navigation',
+                            'depth'             => 2,
+                            'container'         => 'div',
+                            'container_class'   => 'collapse navbar-collapse navText',
+                            'container_id'      => 'top_navigation',
+                            'menu_class'        => 'nav navbar-nav',
+                            'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+                            'walker'            => new WP_Bootstrap_Navwalker(),
+                        ) );
+                        ?>
+                    </div>
                 </div>
-                <div class="links">
-                    <?php
-                    wp_nav_menu( array(
-                        'theme_location'    => 'top_navigation',
-                        'depth'             => 2,
-                        'container'         => 'div',
-                        'container_class'   => 'collapse navbar-collapse navText',
-                        'container_id'      => 'top_navigation',
-                        'menu_class'        => 'nav navbar-nav',
-                        'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
-                        'walker'            => new WP_Bootstrap_Navwalker(),
-                    ) );
-                    ?>
-                </div>
-            </div>
-            <form action="<?php echo home_url();?>" method="get" class="d-flex justify-content-center">
-                <input type="hidden" name="post_type" value="post, movie"></input>
-                <input name="s" type="text" class="form-control" aria-describedby="searchPosts" placeholder="Search Posts" value="<?php the_search_query(); ?>">
-                <button class="btn text-black ml-3 searchButton mr-4" type="submit">Search</button>
-            </form>
-        </nav>
+                <form action="<?php echo home_url();?>" method="get" class="d-flex justify-content-center searchSection">
+                    <input type="hidden" name="post_type" value="post, movie"></input>
+                    <input name="s" type="text" class="form-control" aria-describedby="searchPosts" placeholder="Search Posts" value="<?php the_search_query(); ?>">
+                    <button class="btn text-black ml-3 searchButton mr-4" type="submit">Search</button>
+                </form>
+            </nav>
         <?php endif; ?>
     </div>
-</div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<!-- <?php if (get_theme_mod('2019headerImageDisplay')): ?>
-
-<?php else: ?>
-    <!-- <div class="container">
-        <div class="row">
-            <div class="col text-center">
-                <h1 class="display-3 headerText"><?php echo get_bloginfo('name') ?></h1>
+    <?php if (have_posts()): the_post(); ?>
+        <div class="row m-0">
+            <div class="col d-flex justify-content-center">
+                <div class="aboutSection">
+                    <?php the_content(); ?>
+                </div>
             </div>
         </div>
-    </div> -->
-<?php endif; ?>
+    <?php endif; ?>
 
-<!-- <?php get_footer(); ?> -->
+    <?php if (get_theme_mod('2019servicesBackgroundDisplay')): ?>
+        <div class="row m-0 servicesSection">
+            <img src="<?php echo get_theme_mod('2019servicesBackgroundDisplay');  ?>"class="servicesSection">
+        </div>
+    <?php else: ?>
+        <div class="servicesSection">
+            <h1>this is the services section</h1>
+        </div>
+    <?php endif; ?>
+</div>
+
+<?php get_footer(); ?>
