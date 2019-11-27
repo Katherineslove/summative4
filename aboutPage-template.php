f50a67d237b8c7a4a26b000e89f803b4b73a5ad5
Katherine Love <katherine@Katherines-MacBook-Air.local>
2019-11-28 09:52:07 +1300

    changed the top navigation into columns

--- aboutPage-template.php
+++ aboutPage-template.php
@@ -11,40 +11,43 @@ Template Post Type: page
     <div class="aboutImage">
         <?php if(has_nav_menu( 'top_navigation' )): ?>
             <nav class="navbar navbar-expand-md navbar-light topNAV p-0" role="navigation">
-
-                <div class="logo">
+                <div class="col-3">
                     <?php if (get_theme_mod('2019logoImageDisplay')): ?>
                         <img src="<?php echo get_theme_mod('2019logoImageDisplay');  ?>"class="logoImage img-fluid">
                     <?php else: ?>
                         <h1 class="siteName"><?php echo get_bloginfo('name'); ?></h1>
                     <?php endif; ?>
+
                 </div>
 
-                <button class="navbar-toggler button" type="button" data-toggle="collapse" data-target="#top_navigation" aria-controls="top_navigation" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+                <div class="col-6">
+                    <button class="navbar-toggler button" type="button" data-toggle="collapse" data-target="#top_navigation" aria-controls="top_navigation" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
 
-
-                <div class="links">
-                    <?php
-                    wp_nav_menu( array(
-                        'theme_location'    => 'top_navigation',
-                        'depth'             => 2,
-                        'container'         => 'div',
-                        'container_class'   => 'collapse navbar-collapse navText',
-                        'container_id'      => 'top_navigation',
-                        'menu_class'        => 'nav navbar-nav',
-                        'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
-                        'walker'            => new WP_Bootstrap_Navwalker(),
-                    ) );
-                    ?>
+                    <div class="links">
+                        <?php
+                        wp_nav_menu( array(
+                            'theme_location'    => 'top_navigation',
+                            'depth'             => 2,
+                            'container'         => 'div',
+                            'container_class'   => 'collapse navbar-collapse navText',
+                            'container_id'      => 'top_navigation',
+                            'menu_class'        => 'nav navbar-nav',
+                            'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+                            'walker'            => new WP_Bootstrap_Navwalker(),
+                        ) );
+                        ?>
+                    </div>
                 </div>
 
-                <form action="<?php echo home_url();?>" method="get" class="d-flex justify-content-center searchSection">
-                    <input type="hidden" name="post_type" value="post, movie"></input>
-                    <input name="s" type="text" class="form-control border border-dark" aria-describedby="searchPosts" placeholder="Search Posts" value="<?php the_search_query(); ?>">
-                    <button class="btn text-black ml-3 searchButton mr-4 border border-dark" type="submit">Search</button>
-                </form>
+                <div class="col-3">
+                    <form action="<?php echo home_url();?>" method="get" class="d-flex justify-content-center searchSection">
+                        <input type="hidden" name="post_type" value="post, movie"></input>
+                        <input name="s" type="text" class="form-control border border-dark" aria-describedby="searchPosts" placeholder="Search Posts" value="<?php the_search_query(); ?>">
+                        <button class="btn text-black ml-3 searchButton mr-4 border border-dark" type="submit">Search</button>
+                    </form>
+                </div>
             </nav>
         <?php endif; ?>
     </div>
--- front-page.example.php
+++ front-page.example.php
@@ -4,19 +4,19 @@
     <div class="headerImage">
         <?php if(has_nav_menu( 'top_navigation' )): ?>
             <nav class="navbar navbar-expand-md navbar-light topNAV p-0" role="navigation">
-                <div class="container p-0">
-                    <div class="logo mr-2">
-                        <?php if (get_theme_mod('2019logoImageDisplay')): ?>
-                            <img src="<?php echo get_theme_mod('2019logoImageDisplay');  ?>"class="logo w-100">
-                        <?php else: ?>
-                            <h1 class="siteName"><?php echo get_bloginfo('name'); ?></h1>
-                        <?php endif; ?>
-                    </div>
+                <div class="col-3">
+                    <?php if (get_theme_mod('2019logoImageDisplay')): ?>
+                        <img src="<?php echo get_theme_mod('2019logoImageDisplay');  ?>"class="logoImage img-fluid">
+                    <?php else: ?>
+                        <h1 class="siteName"><?php echo get_bloginfo('name'); ?></h1>
+                    <?php endif; ?>
 
+                </div>
+
+                <div class="col-6">
                     <button class="navbar-toggler button" type="button" data-toggle="collapse" data-target="#top_navigation" aria-controls="top_navigation" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
-
 
                     <div class="links">
                         <?php
@@ -33,15 +33,18 @@
                         ?>
                     </div>
                 </div>
-                <form action="<?php echo home_url();?>" method="get" class="d-flex justify-content-center searchSection">
-                    <input type="hidden" name="post_type" value="post, movie"></input>
-                    <input name="s" type="text" class="form-control" aria-describedby="searchPosts" placeholder="Search Posts" value="<?php the_search_query(); ?>">
-                    <button class="btn text-black ml-3 searchButton mr-4" type="submit">Search</button>
-                </form>
+
+                <div class="col-3">
+                    <form action="<?php echo home_url();?>" method="get" class="d-flex justify-content-center searchSection">
+                        <input type="hidden" name="post_type" value="post, movie"></input>
+                        <input name="s" type="text" class="form-control border border-dark" aria-describedby="searchPosts" placeholder="Search Posts" value="<?php the_search_query(); ?>">
+                        <button class="btn text-black ml-3 searchButton mr-4 border border-dark" type="submit">Search</button>
+                    </form>
+                </div>
             </nav>
         <?php endif; ?>
     </div>
-
+    
     <?php if (have_posts()): the_post(); ?>
         <div class="row m-0">
             <div class="col d-flex justify-content-center">
