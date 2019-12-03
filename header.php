a65726c2a003254d833f485c64cff9dd78f6139d
Katherine Love <katherine@Katherines-MacBook-Air.local>
2019-12-03 15:35:09 +1300

    new index and header

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="description" content="Wordpress redesign of the St John New Zealand Website">
-    <meta name="keywords" content="HTML,CSS,PHP,WordPress">
+    <meta name="keywords" content="HTML,CSS,PHP, WordPress">
     <meta name="author" content="Katherine Love">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>St John New Zealand - Here for Life</title>
--- index.php
+++ index.php
@@ -12,4 +12,4 @@
 
 
 
-<?php get_footer(); ?>
+<?php get_footer();  ?>
