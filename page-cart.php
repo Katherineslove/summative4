5cafef7f5aaf856eb6098ce3b54a2b7a59d3b53a
Katherine Love <katherine@Katherines-MacBook-Air.local>
2019-12-04 00:05:47 +1300

    styled the cart page

---
+++ page-cart.php
@@ -0,0 +1,57 @@
+<?php
+/*
+Template Name: Shop Page
+Template Post Type: page
+*/
+?>
+<?php get_header(); ?>
+
+<div class="container-fluid p-0">
+
+    <?php if(has_nav_menu( 'top_navigation' )): ?>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom border-dark">
+            <div class="logo">
+                <?php if (get_theme_mod('2019logoImageDisplay')): ?>
+                    <img src="<?php echo get_theme_mod('2019logoImageDisplay');  ?>"class="img-fluid logoImage">
+                <?php else: ?>
+                    <a class="navbar-brand" href="index.php"><?php echo get_bloginfo('name'); ?></a>
+                <?php endif; ?>
+            </div>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse text-center" id="navbarTogglerDemo02">
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                    <li>
+                        <?php
+                        wp_nav_menu( array(
+                            'theme_location'    => 'top_navigation',
+                            'depth'             => 2,
+                            'container'         => 'div',
+                            'container_class'   => 'navText',
+                            'container_id'      => 'top_navigation',
+                            'menu_class'        => 'nav navbar-nav',
+                            'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+                            'walker'            => new WP_Bootstrap_Navwalker(),
+                        ) );
+                        ?>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    <?php endif; ?>
+
+    <h1 class="cartTitle border-bottom"><?php echo the_title(); ?></h1>
+
+    <?php if (have_posts()): the_post(); ?>
+        <div class="row m-0">
+            <div class="col d-flex justify-content-center">
+                <div class="aboutSection">
+                    <?php the_content(); ?>
+                </div>
+            </div>
+        </div>
+    <?php endif; ?>
+</div>
+<?php get_footer(); ?>
