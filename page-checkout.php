c734e9a2ccaef5438f5767596655331aebdd8029
Katherine Love <katherine@Katherines-MacBook-Air.local>
2019-12-04 00:39:31 +1300

    checkout page

--- page-checkout.php
+++ page-checkout.php
@@ -36,8 +36,6 @@
         </nav>
     <?php endif; ?>
 
-    <h1 class="shopTitle border-bottom"><?php echo the_title(); ?></h1>
-
     <?php if (have_posts()): the_post(); ?>
         <div class="row m-0">
             <div class="col d-flex justify-content-center">
