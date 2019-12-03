8104e99dd9d6af9f912e79c62f41de56a8fb82bc
Katherine Love <katherine@Katherines-MacBook-Air.local>
2019-12-03 22:05:08 +1300

    styled the dropdown menu

--- front-page.php
+++ front-page.php
@@ -5,10 +5,10 @@
     <div class="headerImage w-100">
         <div class="w-100 h-100" style="background: rgba(255,255,255, 0.3) !important;">
             <?php if(has_nav_menu( 'top_navigation' )): ?>
-                <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
                     <div class="logo">
                         <?php if (get_theme_mod('2019logoImageDisplay')): ?>
-                            <img src="<?php echo get_theme_mod('2019logoImageDisplay');  ?>"class="img-fluid">
+                            <img src="<?php echo get_theme_mod('2019logoImageDisplay');  ?>"class="img-fluid logoImage">
                         <?php else: ?>
                             <a class="navbar-brand" href="index.php"><?php echo get_bloginfo('name'); ?></a>
                         <?php endif; ?>
@@ -17,20 +17,22 @@
                         <span class="navbar-toggler-icon"></span>
                     </button>
 
-                    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                    <div class="collapse navbar-collapse text-center" id="navbarTogglerDemo02">
                         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                            <?php
-                            wp_nav_menu( array(
-                                'theme_location'    => 'top_navigation',
-                                'depth'             => 2,
-                                'container'         => 'div',
-                                'container_class'   => 'navText',
-                                'container_id'      => 'top_navigation',
-                                'menu_class'        => 'nav navbar-nav',
-                                'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
-                                'walker'            => new WP_Bootstrap_Navwalker(),
-                            ) );
-                            ?>
+                            <li>
+                                <?php
+                                wp_nav_menu( array(
+                                    'theme_location'    => 'top_navigation',
+                                    'depth'             => 2,
+                                    'container'         => 'div',
+                                    'container_class'   => 'navText',
+                                    'container_id'      => 'top_navigation',
+                                    'menu_class'        => 'nav navbar-nav',
+                                    'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+                                    'walker'            => new WP_Bootstrap_Navwalker(),
+                                ) );
+                                ?>
+                            </li>
                         </ul>
                     </div>
                 </nav>
@@ -71,8 +73,5 @@
             <?php endif; ?>
         </div>
     <?php endif; ?>
-
-
-
 </div>
 <?php get_footer(); ?>
