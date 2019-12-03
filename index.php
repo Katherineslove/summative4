4e958d51967e75c53ae4d1a320207a8e815e3e2e
Katherine Love <katherine@Katherines-MacBook-Air.local>
2019-12-03 16:37:40 +1300

    working collapse nav

--- index.php
+++ index.php
@@ -7,18 +7,18 @@
             <?php if(has_nav_menu( 'top_navigation' )): ?>
                 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
                     <a class="navbar-brand" href="index.php"><?php echo get_bloginfo('name'); ?></a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
 
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav mr-auto">
+                    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
                             <?php
                             wp_nav_menu( array(
                                 'theme_location'    => 'top_navigation',
                                 'depth'             => 2,
                                 'container'         => 'div',
-                                'container_class'   => 'collapse navbar-collapse navText',
+                                'container_class'   => 'navText',
                                 'container_id'      => 'top_navigation',
                                 'menu_class'        => 'nav navbar-nav',
                                 'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
