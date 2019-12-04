85b476f894a48351660679878fe6734fcf85e843
Katherine Love <katherine@Katherines-MacBook-Air.local>
2019-12-05 08:45:49 +1300

    making live

--- functions.php
+++ functions.php
@@ -2,10 +2,10 @@
 
 function addCustomThemeStyles(){
     wp_enqueue_style("bootstrapCSS2019", get_template_directory_uri() . "/assets/css/bootstrap.min.css", array(), "4.3.1", "all");
-    wp_enqueue_style('customCSS', get_template_directory_uri() . '/assets/css/style.css', array(), '0.0.1', 'all');
+    wp_enqueue_style('customCSS', get_template_directory_uri() . '/assets/css/style.css', array(), '1.0.0', 'all');
     wp_enqueue_script("jquery");
     wp_enqueue_script("bootstrapJS2019", get_template_directory_uri() . "/assets/js/bootstrap.min.js", array("jquery"), "4.3.1", true);
-    wp_enqueue_script("customJS2019", get_template_directory_uri() . "/assets/js/script.js", array("jquery"), "0.0.2", true);
+    wp_enqueue_script("customJS2019", get_template_directory_uri() . "/assets/js/script.js", array("jquery"), "1.0.0", true);
 };
 
 add_action('wp_enqueue_scripts', 'addCustomThemeStyles');
